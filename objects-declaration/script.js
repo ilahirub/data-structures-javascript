83608872db40e3d5d0c6cc5967b544a41f8697e5
ilahirub <ilahirubb@gmail.com>
2025-09-16 00:22:57 +0530

    [updated-objects-declarations]

--- objects-declaration/script.js
+++ objects-declaration/script.js
@@ -27,4 +27,38 @@ console.log(personCourses);
 
 //? bracket notation
 const personHobby = person["hobby"];
-console.log(personHobby);
+// console.log(personHobby);
+
+//! objects.keys
+const keys = Object.keys(person);
+console.log(keys.length);
+
+//! object.values
+const car = { brand: "honda", model: "accord", year: 2025 };
+const values = Object.values(car);
+console.log(values);
+console.log(car);
+
+//! hasOwnProperty (to check properties are there in objects-true/false)
+const hasOwnProperty = Object.hasOwn(car, "brand");
+console.log(hasOwnProperty);
+
+const hasProperty = Object.hasOwnProperty(car, "make");
+console.log(hasProperty);
+
+//! object.assign
+const targetObj = { name: "thomas" };
+const sourceObj = { age: 22, country: "india" };
+const result = Object.assign(targetObj, sourceObj);
+console.log(result);
+
+//!delete operator (remove property from object)
+delete car.brand;
+console.log(car);
+
+//! freeze(making objects immutable no changes in their properties)
+Object.freeze(car);
+
+//modify
+car.make = "nissan";
+console.log(car);
