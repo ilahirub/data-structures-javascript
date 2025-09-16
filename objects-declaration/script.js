ffc20ae38c90a3e38908cca9792a3fc74ecd75d2
ilahirub <ilahirubb@gmail.com>
2025-09-16 14:07:26 +0530

    [updated-script-file]

--- objects-declaration/script.js
+++ objects-declaration/script.js
@@ -62,3 +62,53 @@ Object.freeze(car);
 //modify
 car.make = "nissan";
 console.log(car);
+
+//! Nesting objects
+const user = {
+  name: "alice",
+  age: 22,
+  address: {
+    street: "123 main street",
+    city: "new york",
+    country: { name: "USA", code: "US" },
+  },
+};
+
+console.log(user);
+
+//!Accessing nested object properties
+//? dot notation
+const userName = user.name;
+const userAddress = user.address.street;
+const countryName = user.address.country.name;
+console.log(countryName);
+
+//? bracket notation for selecting or accessing objects properties
+const countryNames = user["address"]["country"]["name"];
+console.log(countryNames);
+
+//! Objects in an array
+const userObj = [
+  { name: "alice", age: 22, city: "sans" },
+  { name: "bob", age: 23, city: "bangalore" },
+  { name: "bobby", age: 34, city: "new york" },
+];
+
+// console.log(userObj);
+
+//! Accessing properties
+const firstUser = userObj[0];
+const secondUser = userObj[1];
+const thirdUser = userObj[2];
+
+//! Add new user to userObj
+userObj.push([{ name: "david", age: 26, city: "chikago" }]);
+console.log(userObj);
+
+userObj.unshift([{ name: "ibraham", age: 30, city: "ghana" }]);
+console.log(userObj);
+
+//! Modify the arrays of object
+//? update user name from ibraham to dawood
+userObj[0].name = "dawood";
+console.log(userObj);
